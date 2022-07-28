c13692a397a34a0ec6446ad201c85eaf6fadd116
Mysto77 <mystogendx@gmail.com>
2022-07-28 10:37:49 +0530

    changed display message

--- mysto.java
+++ mysto.java
@@ -2,7 +2,7 @@ public class dowhile{
 
 	public static void main(String[] args){
 	int i=10,b=20;
-	System.out.println("first commit");
+	System.out.println("sum of two numbers");
 	System.out.println(i+b);
 	}
 }
