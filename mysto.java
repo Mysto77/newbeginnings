46eba6c2b3231f40af0a5d0efccc17b2bc9debbe
Mystogan <105197252+Mysto77@users.noreply.github.com>
2022-07-28 10:47:02 +0530

    Update mysto.java
    changed the operation to multiply

--- mysto.java
+++ mysto.java
@@ -3,6 +3,6 @@ public class dowhile{
 	public static void main(String[] args){
 	int i=10,b=20;
 	System.out.println("sum of two numbers");
-	System.out.println(i+b);
+	System.out.println(i*b);
 	}
 }
